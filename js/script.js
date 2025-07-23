edf78a654d52c90cf4998ba30a4f3a5219acbe68
UELISSON TIAGO SILVA SANTOS <020.732974@universidadesenaicimatec.edu.br>
2025-07-23 16:49:31 -0300

    Adicionado arquivos e estilos

--- js/script.js
+++ js/script.js
@@ -1,15 +1,75 @@
-// Mostre no console cada parágrado do site
-const paragrafos = document.querySelectorAll("p");
-paragrafos.forEach((item) => console.log(item));
-// Mostre o texto dos parágrafos no console
-paragrafos.forEach((item) => console.log(item.textContent));
+function initTabNav() {
+  const tabmenu = document.querySelectorAll(".js-tabMenu li");
+  const tabcontent = document.querySelectorAll(".js-tabContent section");
 
-// Como corrigir os erros abaixo:
-const imgs = document.querySelectorAll("img");
+  function activeTab(index) {
+    tabcontent.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    tabcontent[index].classList.add("ativo");
+  }
 
-imgs.forEach(item, (index) => console.log(item, index));
+  tabmenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", function () {
+      activeTab(index);
+    });
+  });
+}
 
-let i = 0;
-imgs.forEach(() => console.log(i++));
+initTabNav();
 
-imgs.forEach(() => i++);
+function initAccordion() {
+  const accordionItem = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+  if (accordionItem.length) {
+    accordionItem[0].classList.add(activeClass);
+    accordionItem[0].nextElementSibling.classList.add(activeClass);
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    accordionItem.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
+
+function initScroll() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((item) => {
+    item.addEventListener("click", scrollToSection);
+  });
+}
+
+function animationInitScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animationScroll() {
+      sections.forEach((section) => {
+        const sectiontop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectiontop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    animationScroll();
+    window.addEventListener("scroll", animationScroll);
+  }
+}
+animationInitScroll();
