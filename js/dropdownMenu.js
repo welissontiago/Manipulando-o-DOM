348e9fc25b348354e11b86a368eb446d4008b5a6
UELISSON TIAGO SILVA SANTOS <020.732974@universidadesenaicimatec.edu.br>
2025-08-06 16:57:16 -0300

    adicionado dropDown, cursor no mapa e tooltip

---
+++ js/dropdownMenu.js
@@ -0,0 +1,26 @@
+export default function initDropDown() {}
+
+const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+
+dropdownMenus.forEach((menu) => {
+  ["touchstart", "click"].forEach((userEvent) => {
+    menu.addEventListener(userEvent, handleClick);
+  });
+});
+
+function handleClick(event) {
+  event.preventDefault();
+  this.classList.toggle("active");
+  outsideClick(this, () => {
+    this.classList.remove("active");
+  });
+}
+
+function outsideClick(element, callback) {
+  const html = document.documentElement;
+  html.addEventListener("click", handleOutsideClick);
+  function handleOutsideClick(event) {
+    if(!element.contains(event.target))
+    callback();
+  }
+}
--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@ import Accordion from "./Accordion.js";
 import initScroll from "./initScroll.js";
 import animationInitScroll from "./animationInitScroll.js";
 import initModal from "./modal.js";
+import initTooltip from "./tooltip.js";
+import initDropDown from "./dropdownMenu.js";
 
 initModal();
+initDropDown();
 initTabNav();
 Accordion();
 initScroll();
 animationInitScroll();
+initTooltip();
